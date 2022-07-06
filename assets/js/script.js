55f91ea45bf3e3ed2d7094269da18710d875b0cc
Gonzalo Rodriguez <gonzaloostos02@gmail.com>
2022-07-06 15:28:38 -0400

    first test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,66 @@
+var startButton = document.getElementById('start-btn');
+
+startButton.addEventListener("click", startGame);
+
+var quizQuestions = [
+    {
+        question: "Could you name some built-in methods in JavaScript?",
+        answers: {
+            a:"concat",
+            b:"font color",
+            c:"Rich interfaces",
+        }
+    },
+    {
+        question: "Please describe the most important advantage of using JavaScript.",
+        answers: {
+            a:"show images",
+            b:"Enhanced interactivity",
+            c:"larger paragraghs",
+        }
+    },
+    {
+        question: "Please select 1 JavaScript data types.",
+        answers: {
+            a:"height",
+            b:"font color",
+            c:"Undefined",
+        }
+    },
+    {
+        question: "In how many ways can you create an array in JS",
+        answers: {
+            a:"3",
+            b:"any",
+            c:"2",
+        }
+    },
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        answers: {
+            a:"<javascript>",
+            b:"<script",
+            c:"<js>",
+        }
+    }
+];
 
 
 
+//start button
+function startGame(){
+    console.log('started');
+    startButton.classList.add('hide')
 
 
+}
+//show questions
 
-//start button click
-function buildQuiz(){}
+function setNextQuest(){
 
-//start quiz questions...
+}
 
-//submit results, show the scores
+function selectAnswer(){
 
-function showResults(){}
 
-//buildQuiz();
-
-//submitButton.addEventListener('click', showResults);
+}
