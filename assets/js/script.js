11298ebc8663e8728c56076929fb4313aaa34541
Gonzalo Rodriguez <gonzaloostos02@gmail.com>
2022-07-06 14:14:55 -0400

    first push

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+//start button click
+function buildQuiz(){}
+
+//start quiz questions...
+
+//submit results, show the scores
+
+function showResults(){}
+
+//buildQuiz();
+
+//submitButton.addEventListener('click', showResults);
