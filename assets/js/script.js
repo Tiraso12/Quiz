7453949dec3b6da23939316aef4dc531eb83fa49
Gonzalo Rodriguez <gonzaloostos02@gmail.com>
2022-07-07 22:26:43 -0400

    redo of functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,54 +1,60 @@
-var startButton = document.querySelector("#start-btn")
-var prueba = document.querySelector("#questions")
+var startButton = document.getElementById("start-btn")
+var prueba = document.getElementById("start")
+var questions = document.getElementById("quizquestions")
+var hscores = document.getElementById("highscores")
+var timeEl = document.getElementById("time")
+//var time = quizQuestions.length * 15;
+var timerId;
+
 
 var quizQuestions = [
     {
-        question1: "Could you name some built-in methods in JavaScript?",
-        answers: {
-            a:"concat",
-            b:"font color",
-            c:"Rich interfaces",
-            correct: "a"
-        }
-    },
-    {
-        question2: "Please describe the most important advantage of using JavaScript.",
-        answers: {
-            a:"show images",
-            b:"Enhanced interactivity",
-            c:"larger paragraghs",
-            correct: "b"
-        }
-    },
-    {
-        question3: "Please select 1 JavaScript data types.",
-        answers: {
-            a:"height",
-            b:"font color",
-            c:"Undefined",
-            correct: "c"
-        }
-    },
-    {
-        question4: "In how many ways can you create an array in JS",
-        answers: {
-            a:"3",
-            b:"any",
-            c:"2",
-            correct: "3"
-        }
-    },
-    {
-        question5: "Inside which HTML element do we put the JavaScript?",
-        answers: {
-            a:"<javascript>",
-            b:"<script>",
-            c:"<js>",
-            correct: "b"
-        }
+        question: "Could you name some built-in methods in JavaScript?",
+        answers: [
+            "concat",
+            "font color",
+            "Rich interfaces"
+        ],
+        correct: "concat",
     }
+    // {
+    //     question: "Please describe the most important advantage of using JavaScript.",
+    //     answers: {
+    //         "show images",
+    //         "Enhanced interactivity",
+    //         "larger paragraghs",
+    //         correct: "concat"
+    //     }
+    // },
+    // {
+    //     question: "Please select 1 JavaScript data types.",
+    //     answers: {
+    //         "height",
+    //         "font color",
+    //         "Undefined",
+    //         correct: "concat"
+    //     }
+    // },
+    // {
+    //     question: "In how many ways can you create an array in JS",
+    //     answers: {
+    //         "3",
+    //         "any",
+    //         "2",
+    //         correct: "concat"
+    //     }
+    // },
+    // {
+    //     question: "Inside which HTML element do we put the JavaScript?",
+    //     answers: {
+    //         "<javascript>",
+    //         "<script>",
+    //         "<js>",
+    //         correct: "concat"
+    //     }
+    // }
 ];
-
+console.log(quizQuestions.length);
 // start game set timer and score
 // generate question
 // generate option buttons
@@ -56,24 +62,50 @@ var quizQuestions = [
 
 
 //start button
-function startGame(){
-var score = 0;
-var timer = 0;
-setNextQuest();
+function startGame() {
+    
+    prueba.setAttribute("class", 'hide');
+    questions.removeAttribute("class");
+    timerId = setInterval(timer,1000);
+    timeEl.textContent = time;
+    setNextQuest();
+
 
 }
 
 
-function setNextQuest(){
+function setNextQuest() {
     var pregunta = prueba;
-    pregunta.textContent = quizQuestions
+    pregunta.textContent = quizQuestions;
+    startButton.style.display = "none"
+}
 
-    console.log(prueba);
+function selectAnswer() {
+
 
 }
 
-function selectAnswer(){
+function timer() {
+    time--;
+    timeEl.textContent = time;
 
+    if (time <= 0) {
+
+        endGame();
+    }
+
+}
+
+function endGame() {
 
 }
 startButton.addEventListener("click", startGame,)
+
+
+//function 1 hide div
+
+//function 2 create question
+
+//function logic
+
+// function end game (end game, stop timer, condition " less than 0 force it to 0 define score as object")
